4721466c4813f0986e6e03cae1b89a5157dbf3ce
Paramjeet <iampjeetsingh@gmail.com>
2020-12-30 11:35:32 +0530

    Authentication Section Completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const { check } = require('express-validator');
-const {signout, signup} = require('../controllers/auth')
+const {signout, signup, signin, isSignedIn} = require('../controllers/auth')
 
 router.post("/signup",[
     check("name", "name should be atleast 3 characters").isLength({min: 3}),
     check("email", "email is required").isEmail(),
     check("password", "password should be atleast 3 char").isLength({min: 3})
 ], signup);
+
+router.post("/signin",[
+    check("email", "email is required").isEmail(),
+    check("password", "password field is required").isLength({min: 1})
+], signin);
+
 router.get("/signout", signout);
+
+router.get("/testroute", isSignedIn, (req, res)=>{
+    res.send("A protected route")
+})
 
 module.exports = router;
